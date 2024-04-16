e1fcb0f451c0ea027fa35ca3765d32ae9daf4a2a
FelipeSoouza <146121300+FelipeSoouza@users.noreply.github.com>
2024-04-16 08:30:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nome do filme: ");
+            string nomeFilme = Console.ReadLine();
+
+            Console.Write("Digite o nome do cliente: ");
+            string nomeCliente = Console.ReadLine();
+
+            DateTime dataRetirada = DateTime.Now;
+            DateTime dataEntregue = dataRetirada.AddDays(7);
+
+            Console.WriteLine("O filme" + nomeFilme + " foi retirado por " + nomeCliente + " em" + dataRetirada.ToString() + " e deve ser entregue até " + dataEntregue.ToString() + ".");
+        }
+    }
+}
